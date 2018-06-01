4faf6adaa28bbcf3def59b9024c670d779ba6146
Jose Naves <josenaves@gmail.com>
2018-06-01 12:20:13 -0300

    Create Controller for game

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,46 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by José Naves on 31/05/18.
+//  Copyright © 2018 José Naves. All rights reserved.
+//
+
+import Foundation
+
+class Concentration
+{
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int? // optional
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+    }
+    
+    // TODO: shuffle the cards
+}
